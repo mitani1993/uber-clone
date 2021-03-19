895c7ca993c38f1cb4e7bbc477c9b9a72ed6df94
mitani1993 <fytim55@gmail.com>
2021-03-19 12:40:43 +0900

    api

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,6 +1,13 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
+import { fetchLineFoods } from '../apis/line_foods';
 
 export const Orders = () => {
+  useEffect(() => {
+    fetchLineFoods()
+    .then((data) => console.log(data))
+    .catch((e) => console.error(e));
+  }, []);
+
   return (
     <Fragment>
       注文画面
